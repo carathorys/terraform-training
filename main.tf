7a8d3357519fb7693764b38cffa765479430fdd3
Balazs Gallay <balazs.gallay@zooshgroup.com>
2022-04-26 11:38:43 +0200

    Add remote module

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ locals {
   # reference to the module's output
   local_5 = module.my_module.my_module_output
 }
+
+module "same_module_from_github" {
+  # Reference a module from git
+  source               = "git@github.com:carathorys/terraform-training-remote-module.git?ref=1.0.0"
+  remote_modules_input = true # Define input variables
+}
+
+locals {
+  # reference to the module's output
+  local_6 = module.same_module_from_github.remote_modules_output
+}
